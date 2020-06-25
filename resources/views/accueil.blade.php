ec8c9a7a164df065f90312b5304fb93ceb7581df
W-mseg <marco.segretario2705@gmail.com>
2020-06-25 15:00:51 +0200

    put

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -6,19 +6,19 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pinkerton</title>
-    <link rel="stylesheet" href="{{URL::asset('/css/grid.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('/css/marcofall.css')}}">
 </head>
-<body class="wrapper">
+<body class="row">
+
+<div class="d-flex flex-wrap col-md-12">
 
 @foreach($post as $data)
-
-    <article class="cardwrap">
+    <article class="card col-md-3">
         <h3>{{$data['title']}}</h3>
         <img src="{{$data['image_link']}}" alt="{{$data['description']}}">
-            <p>{{$data['description']}}</p>
     </article>
-
 @endforeach
+</div>
 
 </body>
 </html>
