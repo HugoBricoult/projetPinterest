2034dc76fbc5c11a7a61f24fcfb5df9db4ea352b
hugo <hugo.bricoult0521@gmail.com>
2020-06-26 10:21:15 +0200

    fix storage

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -6,7 +6,7 @@
     
         <article class="cardwrap">
             <h3>{{$data['title']}}</h3>
-            <img src="{{asset($data['image_link'])}}" alt="{{$data['description']}}">
+            <img src="{{asset('storage/uploads/dza.png')}}" alt="{{$data['description']}}">
                 <p>{{$data['description']}}</p>
         </article>
     
