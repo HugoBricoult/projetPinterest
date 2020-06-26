feaf97ec66ffb1c1b55be90ed6f499d9c324d7c9
hugo <hugo.bricoult0521@gmail.com>
2020-06-26 10:25:22 +0200

    fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -45,7 +45,7 @@ class PostController extends Controller
             $imageName = $imagePath->getClientOriginalName();
 
             $path = $request->file('file')->storeAs('uploads', $imageName, 'public');
-            $path = '/'.$path;
+            $path = 'storage/'.$path;
             Post::create([
                 'title' => $request['title'],
                 'description' => $request['description'],
